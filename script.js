91b307fb430ff592cb128e55ed2e75b38b7dcdac
Jules H <jules.hemon@etudiant.univ-reims.fr>
2023-10-20 12:20:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+/*onMounted(async () => {
+    const response = await axios.post(
+      "https://shelly-86-eu.shelly.cloud/device/status",
+      {
+        id: "80646F827174",
+        auth_key:
+          "MWRmYzIxdWlk3C255D368FAAA95635EA81B568C136C66526F87F07896B53C91F33B90056C714E55A1E468EF8396B",
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    apiData.value = await response.data.data.device_status;
+    console.log(apiData);
+  });
+*/
+
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toggleButton');
     const image = document.getElementById('image');
